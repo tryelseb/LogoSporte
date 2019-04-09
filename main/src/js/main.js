635fd1be72eb8d98e0a74229d656a8284792d599
olehsova-hyuna <oleh.sova@hyuna.bb>
2019-04-09 17:06:47 +0300

    full 1,2

--- main/src/js/main.js
+++ main/src/js/main.js
@@ -117,32 +117,37 @@
 // });
 
 // // Menu
-$("#navToggle").click(function() {
+$("#navToggle").click(function () {
 
   $(this).toggleClass("active");
   $(".overlay").toggleClass("open");
   $('.main-menu').removeClass("decor-menu");
-    // this line ▼ prevents content scroll-behind
-    $("body").toggleClass("locked");
-    $('.main-menu__list li').on('click', function(){
-      $('.navBurger').removeClass('active');
-      $('#test').removeClass('open');
-      $('body').removeClass('locked');
-      $(".fixed-menu-color").css("color", "");
-    })
-  });
+  // this line ▼ prevents content scroll-behind
+  $("body").toggleClass("locked");
+  $('.main-menu__list li').on('click', function () {
+    $('.navBurger').removeClass('active');
+    $('#test').removeClass('open');
+    $('body').removeClass('locked');
+    $(".fixed-menu-color").css("color", "");
+  })
+});
 // Go to top
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
   var speed = 500,
-  $scrollTop = $('<a href="#" title="go to top" class="scrollTop"><i class="fa fa-angle-double-up"></i></a>').appendTo('body');        
-  $scrollTop.click(function(e){
+    $scrollTop = $('<a href="#" title="go to top" class="scrollTop"><i class="fa fa-angle-double-up"></i></a>').appendTo('body');
+  $scrollTop.click(function (e) {
     e.preventDefault();
-    $( 'html:not(:animated),body:not(:animated)' ).animate({ scrollTop: 0}, speed );
+    $('html:not(:animated),body:not(:animated)').animate({
+      scrollTop: 0
+    }, speed);
   });
-  function show_scrollTop(){
-    ( $(window).scrollTop() > 300 ) ? $scrollTop.fadeIn(600) : $scrollTop.fadeOut(600);
+
+  function show_scrollTop() {
+    ($(window).scrollTop() > 300) ? $scrollTop.fadeIn(600): $scrollTop.fadeOut(600);
   }
-  $(window).scroll( function(){ show_scrollTop(); } );
+  $(window).scroll(function () {
+    show_scrollTop();
+  });
   show_scrollTop();
 });
 
@@ -185,29 +190,36 @@ jQuery(document).ready(function($){
 
 $(document).ready(function () {
   function windowSize() {
-    if ($(window).width() <= '760') {
-      $('#owl-one').owlCarousel({
-        items: 6,
-        dots: false,
-        lazyLoad: true,
-        responsive: {
-          0: {
-            items: 1,
-            loop: false,
-            center: true,
-            loop: true,
-            autoWidth: true
-          },
-          784: {
-            items: 2,
-            loop: false,
-            center: true,
-            loop: true,
-            autoWidth: true
-          }
+    $('#owl-one').owlCarousel({
+      items: 6,
+      dots: false,
+      lazyLoad: true,
+      autoplay: true,
+      autoplayTimeout: 6000,
+      responsive: {
+        0: {
+          items: 1,
+          center: true,
+          loop: true,
+          autoWidth: true
+        },
+        784: {
+          items: 2,
+          center: true,
+          loop: true,
+          autoWidth: true
+        },
+        1000: {
+          items: 6,
+          center: true,
+          loop: true,
+          autoWidth: true
         }
-      });
-    }
+      }
+    });
   }
+  $(".btn-lg").click(function () {
+    $(".wrp-popup").toggleClass('activ');
+  });
   $(window).on('load resize', windowSize);
 });
